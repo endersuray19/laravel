f14b7630ba798c7503b45ebfaab905211c780ceb
endersurya19@gmail.com <endersurya19@gmail.com>
2026-03-07 10:17:48 +0800

    update  2 tagihan iuran

--- resources/views/admin/tagihan_iuran/edit.blade.php
+++ resources/views/admin/tagihan_iuran/edit.blade.php
@@ -23,13 +23,8 @@
                     <label for="nama_anggota">Nama Anggota</label>
                     <select name="anggota_id" id="anggota_id" class="form-control select2">
                         <option >--Pilih Anggota--</option>
-                        
                             @foreach($anggota as $ang)
-                                <option value="{{$ang->kode_anggota}}" >
-                               
-                                {{
-                                $ang->nama_anggota
-                                }}</option>
+                                <option value="{{$ang->kode_anggota}}" {{$ang->kode_anggota == $tagihan_iuran->anggota_id ? 'selected' : ''}}>{{$ang->nama_anggota}}</option>
                             @endforeach
                     </select>
                 </div>
@@ -43,11 +38,18 @@
                 </div>
                 <div class="form-group">
                     <label for="jenis_iuran_id">Jenis Iuran</label>
-                    <input type="text" name="jenis_iuran_id" class="form-control" value="{{$tagihan_iuran->jenis_iuran_id}}">
+                    <select name="jenis_iuran_id" id="jenis_iuran_id" class="form-control">
+                        @foreach($jenis_iuran as $jn)
+                        <option value="{{$jn->id}}" {{$jn->id == $tagihan_iuran->jenis_iuran_id ? 'selected':''}}>{{$jn->nama_iuran}}</option>
+                        @endforeach
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="status">Status</label>
-                    <input type="text" name="status" class="form-control" value="{{$tagihan_iuran->status}}">
+                    <select name="status" id="status" class="form-control">
+                        <option value="lunas" {{$tagihan_iuran->status == 'lunas' ? 'selected': ''}}>Lunas</option>
+                        <option value="belum_lunas" {{$tagihan_iuran->status == 'belum_lunas' ? 'selected': ''}}>Belum Lunas</option>
+                    </select>
                 </div>
                 <div class="form-group">
                     <a class="btn btn-secondary" href="{{route('admin.tagihan_iuran.index')}}">Kembali</a>
